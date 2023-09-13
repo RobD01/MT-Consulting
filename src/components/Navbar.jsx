384dbfc25ef4bb315effd3d109f928155680f089
RobD01 <101437001+RobD01@users.noreply.github.com>
2023-09-13 16:29:36 -0700

    Update Navbar.jsx / links for custom domain

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,18 +7,18 @@ const Navigation = () => {
     <>
       <Navbar sticky="top" expand="lg" className="mt-1 mb-4 mx-auto navStyle">
         <Container>
-          <Navbar.Brand href="/MT-Consulting/#/">Money Theory</Navbar.Brand>
+          <Navbar.Brand href="/#/">Money Theory</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/MT-Consulting/#/">Home</Nav.Link>
-              <Nav.Link href="/MT-Consulting/#/appointment">
+              <Nav.Link href="/#/">Home</Nav.Link>
+              <Nav.Link href="/#/appointment">
                 Appointment
               </Nav.Link>
-              <Nav.Link href="/MT-Consulting/#/message">Message</Nav.Link>
-              <Nav.Link href="/MT-Consulting/#/community">Community</Nav.Link>
-              <Nav.Link href="/MT-Consulting/#/learn">Learn</Nav.Link>
-              <Nav.Link href="/MT-Consulting/#/merch">Merch</Nav.Link>
+              <Nav.Link href="/#/message">Message</Nav.Link>
+              <Nav.Link href="/#/community">Community</Nav.Link>
+              <Nav.Link href="/#/learn">Learn</Nav.Link>
+              <Nav.Link href="/#/merch">Merch</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
